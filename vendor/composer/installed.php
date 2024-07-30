c72bd81d01518fcca49258f76bc2c7c06e08f2d4
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-07-30 22:54:19 +0800

    alright

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'smcc/research-hub',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e9cbb0d6c5d15a11b7451056d06b49469b30a056',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'smcc/research-hub' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e9cbb0d6c5d15a11b7451056d06b49469b30a056',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
