926aca12c7a157d6689539730550dcecfbda5e29
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-07-31 12:24:46 +0800

    with footer and icon

--- src/Views/Global/HeadTemplate.php
+++ src/Views/Global/HeadTemplate.php
@@ -14,7 +14,7 @@ class HeadTemplate
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-
+      <link rel="shortcut icon" href="/assets/favicon.ico" type="image/x-icon"/>
       <title><?php echo $title . " - " . APP_TITLE; ?></title>
 
       <!-- Google font -->
@@ -35,7 +35,7 @@ class HeadTemplate
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-
+      <link rel="shortcut icon" href="/assets/favicon.ico" type="image/x-icon"/>
       <title><?php echo $title . " - " . APP_TITLE; ?></title>
 
       <!-- Google font -->
