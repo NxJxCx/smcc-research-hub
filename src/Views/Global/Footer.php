5dc5a7b016fb80942895c9d08da452e18c900a62
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-08-06 13:52:28 +0800

    wew

--- src/Views/Global/Footer.php
+++ src/Views/Global/Footer.php
@@ -5,31 +5,31 @@ namespace Smcc\ResearchHub\Views\Global;
 class Footer {
   public static function default() {
 ?>
-    <footer class="font-[Roboto] font-[400] text-[16px] leading-[22.4px] w-full min-h-[100px] grid md:grid-cols-2 items-center justify-end">
-        <div class="flex text-center items-center justify-center">
-          &copy; 2024 Saint Michael College of Caraga
-        </div>
-        <ul class="flex flex-col md:flex-row justify-between gap-2 items-center px-8 max-w-[700px]">
-          <li>
-            <a href="#">Terms</a>
-          </li>
-          <li>
-            <a href="#">Privacy</a>
-          </li>
-          <li>
-            <a href="#">Support</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Resources</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </div>
-    </footer>
+  <footer class="font-[Roboto] font-[400] text-[16px] leading-[22.4px] w-full min-h-[100px] max-w-full mb-4 md:mb-0 grid grid-cols-1 md:grid-cols-2 gap-y-2 items-center justify-end">
+    <div class="flex text-center items-center justify-center">
+      &copy; 2024 Saint Michael College of Caraga
+    </div>
+    <ul class="flex flex-wrap md:flex-nowrap max-w-full justify-center gap-x-4 gap-y-2 md:justify-between md:gap-2 items-center px-8 md:max-w-[700px]">
+      <li>
+        <a href="#">Terms</a>
+      </li>
+      <li>
+        <a href="#">Privacy</a>
+      </li>
+      <li>
+        <a href="#">Support</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">Resources</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+    </div>
+  </footer>
 <?php
   }
 }
