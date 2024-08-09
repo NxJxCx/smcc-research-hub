b67b8d3f65af0e5d12a3104ac7ff164fe1835eb2
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-08-09 15:09:53 +0800

    404

--- jsxbuild/main.js
+++ jsxbuild/main.js
@@ -18,15 +18,14 @@ async function render() {
         root.render(React.createElement(App, null));
     }
     catch (error) {
-        ["https://fonts.googleapis.com/css?family=Nunito:400,700", "/css/style.css"].forEach(addLinkElement);
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        root.render(React.createElement("div", { id: "notfound" },
-            React.createElement("div", { className: "notfound" },
-                React.createElement("div", { className: "notfound-404" }),
-                React.createElement("h1", null, "404"),
-                React.createElement("h2", null, "Oops! Page Not Be Found"),
-                React.createElement("p", null, "Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable"),
-                React.createElement("a", { href: "/" }, "Back to homepage"))));
+        addLinkElement("https://fonts.googleapis.com/css?family=Nunito:400,700");
+        root.render(React.createElement("div", { className: "relative h-full" },
+            React.createElement("div", { className: "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[560px] w-full leading-[1.1] px-[15px] pt-[110px] md:pl-[160px] md:pt-0 md:pr-0" },
+                React.createElement("div", { className: "absolute left-0 top-0 inline-block w-[110px] h-[110px] md:w-[140px] md:h-[140px] bg-[url('/images/emoji.png')] bg-cover" }),
+                React.createElement("h1", { className: "font-[Nunito] text-[65px] font-[700] mt-0 mb-[10px] text-[#151723] uppercase" }, "404"),
+                React.createElement("h2", { className: "font-[Nunito] text-[21px] font-[400] m-0 uppercase text-[#151723]" }, "Oops! Page Not Be Found"),
+                React.createElement("p", { className: "font-[Nunito] font-[400] text-[#999fa5]" }, "Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable"),
+                React.createElement("a", { href: "/", className: "font-[Nunito] text-[#388dbc] rounded-[40px] font-[700] inline-block" }, "Back to homepage"))));
     }
 }
 render();
--- src/Views/react/src/main.tsx
+++ src/Views/react/src/main.tsx
@@ -20,16 +20,15 @@ async function render() {
     const App = (await import(jsxAppPath as string))?.default;
     root.render(<App />);
   } catch (error) {
-    ["https://fonts.googleapis.com/css?family=Nunito:400,700", "/css/style.css"].forEach(addLinkElement);
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    addLinkElement("https://fonts.googleapis.com/css?family=Nunito:400,700");
     root.render(
-      <div id="notfound">
-        <div className="notfound">
-          <div className="notfound-404"></div>
-          <h1>404</h1>
-          <h2>Oops! Page Not Be Found</h2>
-          <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
-          <a href="/">Back to homepage</a>
+      <div className="relative h-full">
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[560px] w-full leading-[1.1] px-[15px] pt-[110px] md:pl-[160px] md:pt-0 md:pr-0">
+          <div className="absolute left-0 top-0 inline-block w-[110px] h-[110px] md:w-[140px] md:h-[140px] bg-[url('/images/emoji.png')] bg-cover"></div>
+          <h1 className="font-[Nunito] text-[65px] font-[700] mt-0 mb-[10px] text-[#151723] uppercase">404</h1>
+          <h2 className="font-[Nunito] text-[21px] font-[400] m-0 uppercase text-[#151723]">Oops! Page Not Be Found</h2>
+          <p className="font-[Nunito] font-[400] text-[#999fa5]">Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
+          <a href="/" className="font-[Nunito] text-[#388dbc] rounded-[40px] font-[700] inline-block">Back to homepage</a>
         </div>
       </div>
     )
