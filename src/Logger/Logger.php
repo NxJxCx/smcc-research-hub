b768e8fd63ffc5e9e963a2592863ee0122697b84
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-08-09 14:13:58 +0800

    with timestamp

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -8,28 +8,32 @@ class Logger
 {
   static public function write_error(string $message)
   {
-    $txt = "[USER ERROR] $message";
+    $timestamp = date('Y-m-d H:i:s');
+    $txt = "[USER ERROR] [$timestamp] $message";
     error_log($txt, 3, "php://stderr");
     error_log($txt . PHP_EOL, 3, LOGGER_FILE_PATH);
   }
   static public function write_warning(string $message)
   {
-    $txt = "[USER WARNING] $message";
+    $timestamp = date('Y-m-d H:i:s');
+    $txt = "[USER WARNING] [$timestamp] $message";
     error_log($txt);
     error_log($txt . PHP_EOL, 3, LOGGER_FILE_PATH);
   }
   static public function write_info(string $message)
   {
-    $txt = "[USER INFO] $message";
+    $timestamp = date('Y-m-d H:i:s');
+    $txt = "[USER INFO] [$timestamp] $message";
     error_log($txt);
     error_log($txt . PHP_EOL, 3, LOGGER_FILE_PATH);
   }
   static public function write_debug(string $message)
   {
     if (PHP_ENV === 'development') {
-      $txt = "[USER DEBUG] $message";
+      $timestamp = date('Y-m-d H:i:s');
+      $txt = "[USER DEBUG] [$timestamp] $message";
       error_log($txt);
-      error_log($txt . PHP_EOL, 3,LOGGER_FILE_PATH);
+      error_log($txt . PHP_EOL, 3, LOGGER_FILE_PATH);
     }
   }
 
